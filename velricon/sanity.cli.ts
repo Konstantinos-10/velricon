1d536ffedec3031685f1c30230b51303fd4a9e50
Konstantinos-10 <t8220215@aueb.gr>
2026-01-23 13:11:45 +0200

    Removed unspported deployment block from the sanity cli

--- velricon/sanity.cli.ts
+++ velricon/sanity.cli.ts
@@ -4,12 +4,5 @@ export default defineCliConfig({
   api: {
     projectId: 'u1w5xzjq',
     dataset: 'production'
-  },
-  deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
-    autoUpdates: true,
   }
 })
